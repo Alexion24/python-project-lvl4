00a39e321061a25923691d73fbccd4c8c0f7baf7
Alexion <alex24bryant@mail.ru>
2022-07-18 19:52:54 +0500

    fix mixins.py

--- task_manager/users/mixins.py
+++ task_manager/users/mixins.py
@@ -9,7 +9,7 @@ class UserMixin(LoginRequiredMixin, UserPassesTestMixin):
     def test_func(self):
         return self.kwargs['pk'] == self.request.user.id
 
-    def not_have_permission(self):
+    def handle_no_permission(self):
         if self.request.user.is_authenticated:
             messages.error(
                 self.request,
