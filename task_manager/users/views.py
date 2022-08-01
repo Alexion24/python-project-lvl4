ee79b0875574fefcf0f28dade34e164612b24ffc
Alexion <alex24bryant@mail.ru>
2022-08-01 17:36:01 +0500

    fix success messages

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -51,10 +51,11 @@ class UpdateUserView(SuccessMessageMixin, UserMixin, UpdateView):
         return context
 
 
-class DeleteUserView(UserMixin, DeleteView):
+class DeleteUserView(SuccessMessageMixin, UserMixin, DeleteView):
     model = User
     template_name = 'delete.html'
     success_url = reverse_lazy('users:users')
+    success_message = _('User successfully deleted.')
 
     def form_valid(self, form):
         if self.get_object().author.all() or self.get_object().executor.all():
